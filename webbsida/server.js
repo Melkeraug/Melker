ec87817f1ff9bed891d79a1ff01c55118fcbd4f0
Melkeraug <54627523+Melkeraug@users.noreply.github.com>
2019-10-23 14:05:05 +0200

    step 3 classrom

---
+++ webbsida/server.js
@@ -0,0 +1,21 @@
+const { createServer } = require("http");
+const { createReadStream } = require("fs");
+
+const sendFile = (response,status, type, filePath) =>{
+    response.writeHead(status, { "Content-Type": type });
+    createReadStream(filePath).pipe(response);
+};
+
+createServer((request, response) => {
+
+    switch (request.url) {
+        case"/":
+          return sendFile(response, 200, "text/html", "./hostahemsida.html");
+        case"/style.css":
+          return sendFile(response, 200, "text/css", "./style.css");
+        case"/volvo240.jpg":
+          return sendFile(response, 200, "image/jpeg","./volvo240.jpg");
+    }
+}).listen(8080);
+
+console.log("Melker's personal website running on port 8080");
